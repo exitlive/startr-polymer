4f4169050c16e13a19feeed066e86694107eb5d9
Matias Meno <m@tias.me>
2016-05-09 16:07:46 +0200

    Fix

--- __filename__.dart
+++ __filename__.dart
@@ -1,11 +1,11 @@
-@HtmlImport('__file_name__.html')
-library exitlive.web.__file_name__;
+@HtmlImport('__filename__.html')
+library exitlive.web.__filename__;
 
 import 'package:logging/logging.dart';
 import 'package:polymer/polymer.dart';
 import 'package:web_components/web_components.dart' show HtmlImport;
 
-@PolymerRegister('__module-name__')
+@PolymerRegister('__modulename__')
 class __ClassName__ extends PolymerElement {
   __ClassName__.created() : super.created();
 
